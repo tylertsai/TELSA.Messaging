24208145c0c20a2df423fbf78d2acd5c525a7151
Tyler Tsai <tsai.chiatai@gmail.com>
2020-08-24 00:05:22 +0800

    Define LineMessagingError and LineMessagingException classes

---
+++ TELSA.Messaging/TELSA.Messaging/LINE/LineMessagingError.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace TELSA.Messaging.LINE
+{
+    /// <summary>
+    /// LINE Messaging API error.
+    /// </summary>
+    public class LineMessagingError
+    {
+        /// <summary>
+        /// Detail.
+        /// </summary>
+        public class Detail
+        {
+            /// <summary>
+            /// Message.
+            /// </summary>
+            public string Message { get; set; }
+
+            /// <summary>
+            /// Property.
+            /// </summary>
+            public string Property { get; set; }
+        }
+
+        /// <summary>
+        /// Message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Details.
+        /// </summary>
+        public IEnumerable<Detail> Details { get; set; }
+    }
+}
---
+++ TELSA.Messaging/TELSA.Messaging/LINE/LineMessagingException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+
+namespace TELSA.Messaging.LINE
+{
+    /// <summary>
+    /// Define an error of LINE Messaging API.
+    /// </summary>
+    public class LineMessagingException : ApplicationException
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="statusCode">HTTP Status Code.</param>
+        /// <param name="message">Error Message.</param>
+        /// <param name="error">Error content.</param>
+        public LineMessagingException(HttpStatusCode statusCode, string message, LineMessagingError error = null)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            Error = error;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="statusCode">HTTP Status Code.</param>
+        /// <param name="message">Error Message.</param>
+        /// <param name="innerException">Inner Exception.</param>
+        /// <param name="error">Error content.</param>
+        public LineMessagingException(HttpStatusCode statusCode, string message, Exception innerException, LineMessagingError error = null)
+            : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+
+        /// <summary>
+        /// HTTP Status Code.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; }
+
+        /// <summary>
+        /// Error content.
+        /// </summary>
+        public LineMessagingError Error { get; }
+    }
+}
